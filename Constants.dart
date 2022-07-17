d6d1561fe1d69262c643650fd0923e9cae279781
Ganesh s s <108235329+ganeshssit@users.noreply.github.com>
2022-07-17 19:08:18 +0530

    Add files via upload

---
+++ Constants.dart
@@ -0,0 +1,15 @@
+import 'main.dart';
+import 'package:flutter/material.dart';
+const KBottomContainerHeight=80.0;
+const KActiveContaineraColour=Color(0xFF1D1E33);
+const KBottomContainerColour=Color(0xFFEB1555);
+const KActiveGestureColur=Color(0xFF111328);
+const KLabelTextStyle=TextStyle(fontSize: 18.0,
+  color: Color(0xFF8D8E98),);
+const KNumberTextStyle =TextStyle(fontSize: 50.0,fontWeight:FontWeight.w900,);
+const KlargeNumberStyle=TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold);
+const KtitleTextSyle=TextStyle(fontSize: 50.0,fontWeight: FontWeight.bold);
+const KresultTextStyle=TextStyle(color: Color(0xFF24D876),fontSize: 22.0,fontWeight:
+FontWeight.bold, );
+const KBMITextStyle=TextStyle(fontSize: 100.0,fontWeight: FontWeight.bold,);
+const KBodyTextStyle=TextStyle(fontSize: 22.0,);
---
+++ calculater_brain.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+class CalculatorBrain{
+  CalculatorBrain({this.height,this.weight});
+
+  final int height;
+  final int weight;
+
+  double _bmi;
+
+  String calculatorBMI(){
+    _bmi=weight/pow(height/100,2);
+    return _bmi.toStringAsFixed(1);
+  }
+String getResult(){
+    if(_bmi>=25){
+      return 'Overweight';
+    } else if(_bmi>18.5){
+      return 'normal';
+    } else{
+      return 'Underweight';
+    }
+}
+String getInterpretation(){
+  if(_bmi>=25){
+    return 'You have ahigher than normal body weight.try to exercise more';
+  } else if(_bmi>18.5){
+    return 'you have normal body weight Good job';
+  } else{
+    return 'You have a lower than a normal body weight.yoy can eat a bit more';
+  }
+
+}
+}
